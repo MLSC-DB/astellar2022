d7a90509962e92e3a468556a450a43cd42665909
shouryade <shourya.de12@gmail.com>
2022-10-05 15:41:02 +0530

    removed visible scrollbar
    Signed-off-by: shouryade <shourya.de12@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -6,15 +6,13 @@ COPY package*.json /usr/src/app/
 
 RUN npm install
 
-
-
 COPY . /usr/src/app/
-
 
 
 # Stage 2 - the production environment
 
 FROM nginx:1.12-alpine
+
 COPY ./nginx/astellar.conf /etc/nginx/nginx.conf
 
 RUN rm -rf /usr/share/nginx/html/*
--- src/Image.js
+++ src/Image.js
@@ -1,13 +1,12 @@
 import React from "react";
 import bgImage from "./Assets/bg.png";
 
-const Image=()=>{
-    return(
-        <div className="bcs">
-            <img src={bgImage}></img>
-        </div>
-        
-    )
-}
+const Image = () => {
+  return (
+    <div className="bcs">
+      <img src={bgImage}></img>
+    </div>
+  );
+};
 
 export default Image;
