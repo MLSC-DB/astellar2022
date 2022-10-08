a442a954c7d7b76865c65ffd07c5ca05db4ca570
shouryade <shourya.de12@gmail.com>
2022-10-09 00:05:07 +0530

    :eyes: play coming soon
    Signed-off-by: shouryade <shourya.de12@gmail.com>

---
+++ src/components/play/Playc.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Countdown from "../Countdown";
+import { useState } from "react";
+
+const Playc = (props) => {
+  return (
+    <>
+      <Container fluid className="home-section" id="home">
+        <Container className="home-content">
+          <Row>
+            <Col md={12} className="heading">
+              <h1
+                style={{
+                  fontSize: "5rem",
+                  fontWeight: "600",
+                  letterSpacing: "0.1rem",
+                  textAlign:"center",
+                }}
+              >
+                Astellar
+              </h1>
+              </Col>
+              <h1 style={{ fontSize: "3rem", fontWeight: "600", textAlign:"center" }}> starts in
+                
+              </h1>
+            
+          </Row>
+          <Countdown />
+        </Container>
+      </Container>
+    </>
+  );
+};
+
+export default Playc;
