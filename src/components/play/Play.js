12b104833928108b456e73f5768c319647ddb451
shouryade <shourya.de12@gmail.com>
2022-10-15 18:40:29 +0530

    changes

--- src/components/play/Play.js
+++ src/components/play/Play.js
@@ -85,18 +85,7 @@ const Play = () => {
         { headers }
       )
       .then((res) => {
-        toast.success(res.data, {
-          position: "top-center",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-        });
-        setTimeout(function () {
-          window.location.reload();
-        }, 3000);
+        navigate("/play")
       })
       .catch((err) => {
         var msg = "";
